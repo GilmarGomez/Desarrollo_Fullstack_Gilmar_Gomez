abea33580a4f579675503bc0c56f79a41c56ac0d
unknown <gilmargomez591@gmail.com>
2024-06-27 21:11:24 -0500

    inicio

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, mundo!");
+        System.out.println("¡Hola!");
+        System.out.println("¡Hola, mundo!");
+        set PATH_TO_FX="path\to\javafx-sdk-21.0.1\lib"
+        
+    }
+}
